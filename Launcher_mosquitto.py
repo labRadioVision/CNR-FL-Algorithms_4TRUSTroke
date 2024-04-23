7f882400576de2ced402c6040a3af56d9ed8f19f
savazzi <stefano.savazzi@ieiit.cnr.it>
2024-04-23 17:51:05 +0200

    run config

--- Launcher_mosquitto.py
+++ Launcher_mosquitto.py
@@ -1,5 +1,5 @@
 import os
 # mosquitto launcher (change dir)
 
-os.chdir(R"c:\Program Files\mosquitto2")
+os.chdir(R"c:\Users\Amministratore\Desktop\CNR-FL-Algorithms_4TRUSTroke\mosquitto2")
 os.system("mosquitto.exe -v -p 1885")
